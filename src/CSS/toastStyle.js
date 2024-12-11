87e4b0a118b1088855a24c28abf42a463340d91b
ShubhamKumar <shubhamk@saxon.ai>
2024-12-11 17:47:31 +0530

    added custom toast style globaly

---
+++ src/CSS/toastStyle.js
@@ -0,0 +1,9 @@
+export const toastStyle={
+    position: "top-right", // Notification position
+    autoClose: 3000,       // Auto close after 3 seconds
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+  }
